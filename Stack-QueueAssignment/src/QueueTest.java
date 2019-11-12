7ca7263346365532690857405f78eb41644258c8
jduda27 <56557345+jduda27@users.noreply.github.com>
2019-11-11 18:25:36 -0600

    init

---
+++ Stack-QueueAssignment/src/QueueTest.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class QueueTest {
+
+	public static void main(String[] args) {
+
+    Stack myStack = new Stack();
+    
+	/** Here is a sample test case. You can create your own test cases here. */
+	myStack.push(1);
+    myStack.push(2);
+    myStack.pop();
+    myStack.push(3);
+	myStack.isEmpty();
+	myStack.push(4);
+	myStack.pop();
+	myStack.push(4);
+	
+	//print final queue after above operations.
+
+  }  // end main
+}  // end QueueTest
---
+++ Stack-QueueAssignment/src/Stack.java
@@ -0,0 +1,28 @@
+class Stack {
+	private QueueReferenceBased queue1;
+	private QueueReferenceBased queue2;
+	
+    /** Initialize your Stack. */
+    public Stack() {
+        queue1 = new QueueReferenceBased();
+        queue2 = new QueueReferenceBased();
+    }
+
+
+    /** Push element x onto stack. */
+    public void push(Object x) {
+        //Your implementation of push( ) operation goes here.
+    }
+    
+	/** Pop the latest added element out of the stack. */
+    public Object pop() {
+		//Your implementation of pop( ) operation goes here.
+
+    }
+    
+	/** Check if the stack is empty. */
+    public boolean isEmpty() {
+		//Your implementation of isEmpty( )  goes here.
+    }
+
+}
