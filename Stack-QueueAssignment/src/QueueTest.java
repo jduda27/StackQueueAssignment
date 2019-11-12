61e49393c31d14a0be562004831b1cf9d90ca023
jduda27 <56557345+jduda27@users.noreply.github.com>
2019-11-11 20:26:44 -0600

    update

--- Stack-QueueAssignment/src/QueueTest.java
+++ Stack-QueueAssignment/src/QueueTest.java
@@ -1,18 +1,28 @@
+/** Stack Implemented With Queues - Assignment 4
+ *  Implement a stack using the textbook Queue implementations
+ *  @author Joshua Duda
+ *  @date 11/11/2019
+ */
+
 public class QueueTest {
 
 	public static void main(String[] args) {
+		
+		//create a new stack
+		Stack myStack = new Stack();
+		//test if the stack is empty
+		System.out.println("It is " + myStack.isEmpty() + " that this stack is empty");
+		myStack.push(1);
+		myStack.push(2);
+		myStack.pop();
+		myStack.push(3);
+		//test if the stack is empty
+		System.out.println("It is " + myStack.isEmpty() + " that this stack is empty");
+		myStack.push(4);
+		myStack.pop();
+		myStack.push(4);
 
-    Stack myStack = new Stack();
-    System.out.println("It is "+myStack.isEmpty()+" that this stack is empty");
-	myStack.push(1);
-    myStack.push(2);
-    myStack.pop();
-    myStack.push(3);
-	System.out.println("It is "+myStack.isEmpty()+" that this stack is empty");
-	myStack.push(4);
-	myStack.pop();
-	myStack.push(4);
-
-	myStack.printQNC();
-  }
+		//print the results and clear the stack.
+		myStack.printQNC();
+	}
 }
--- Stack-QueueAssignment/src/Stack.java
+++ Stack-QueueAssignment/src/Stack.java
@@ -1,3 +1,9 @@
+/** Stack Implemented With Queues - Assignment 4
+ *  Implement a stack using the textbook Queue implementations
+ *  @author Joshua Duda
+ *  @date 11/11/2019
+ */
+
 class Stack {
 	private QueueReferenceBased q1;
 	private QueueReferenceBased q2;
@@ -8,24 +14,33 @@ class Stack {
 		q2 = new QueueReferenceBased();
 	}
 
-	/** Push element x onto stack. */
+	/**
+	 * Push element x onto stack. Precondition: Object x that will be pushed to the
+	 * front of the queue Postcondition: The updated stack with x at the top.
+	 */
 	public void push(Object x) {
+		// add x to a temp queue
 		q2.enqueue(x);
-		while (!q1.isEmpty()) {
-			q2.enqueue(q1.peek());
-			q1.dequeue();
+		while (!q1.isEmpty()) { // while the original queue is not empty
+			q2.enqueue(q1.peek()); // add the value at the front of the queue behind q1
+			q1.dequeue(); // remove the item from q1
 		}
-		q1 = q2;
-		q2 = new QueueReferenceBased();
-		System.out.println(q1.peek());
+		q1 = q2; // set q1 to the temporary queue
+		q2 = new QueueReferenceBased(); // reset q2 to be an empty queue
 	}
 
-	/** Pop the latest added element out of the stack. */
+	/**
+	 * Pop the latest added element out of the stack. Precondition: none
+	 * Postcondition: returns the stack with the top removed.
+	 */
 	public Object pop() {
-		return q1.dequeue();
+		return q1.dequeue(); // remove the value in the front from the stack
 	}
 
-	/** Check if the stack is empty. */
+	/**
+	 * isEmpty() Method Check if the stack is empty. Preconditions: none
+	 * Postconditions: true if the stack is empty, false if the stack is not empty
+	 */
 	public boolean isEmpty() {
 		if (q1.isEmpty()) {
 			return true;
@@ -33,14 +48,16 @@ class Stack {
 			return false;
 		}
 	}
-	/** Print out the queue and clear it.
-	 *  Preconditions: none
-	 *  Postconditions: print out every item in the queue and clears it.
+
+	/**
+	 * Print out the queue and clear it. Preconditions: none Postconditions: print
+	 * out every item in the queue and clears it.
 	 */
 	public void printQNC() {
-		while (!q1.isEmpty()) {
+		// check if the queue is empty
+		while (!q1.isEmpty()) { // while not empty print the top of the stack
 			System.out.print(q1.peek());
-			q1.dequeue();
+			q1.dequeue(); // dequeue the item from the top
 		}
 	}
 
