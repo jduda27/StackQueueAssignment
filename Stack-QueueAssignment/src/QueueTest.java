7446adbda38fe1863f8384ae1f42a8e5a7fb6968
jduda27 <56557345+jduda27@users.noreply.github.com>
2019-11-11 19:06:49 -0600

    update

--- Stack-QueueAssignment/src/QueueTest.java
+++ Stack-QueueAssignment/src/QueueTest.java
@@ -1,22 +1,18 @@
-import java.util.Arrays;
-
 public class QueueTest {
 
 	public static void main(String[] args) {
 
     Stack myStack = new Stack();
-    
-	/** Here is a sample test case. You can create your own test cases here. */
+    System.out.println("It is "+myStack.isEmpty()+" that this stack is empty");
 	myStack.push(1);
     myStack.push(2);
     myStack.pop();
     myStack.push(3);
-	myStack.isEmpty();
+	System.out.println("It is "+myStack.isEmpty()+" that this stack is empty");
 	myStack.push(4);
 	myStack.pop();
 	myStack.push(4);
-	
-	//print final queue after above operations.
 
-  }  // end main
-}  // end QueueTest
+	myStack.printQNC();
+  }
+}
--- Stack-QueueAssignment/src/Stack.java
+++ Stack-QueueAssignment/src/Stack.java
@@ -1,28 +1,47 @@
 class Stack {
-	private QueueReferenceBased queue1;
-	private QueueReferenceBased queue2;
-	
-    /** Initialize your Stack. */
-    public Stack() {
-        queue1 = new QueueReferenceBased();
-        queue2 = new QueueReferenceBased();
-    }
+	private QueueReferenceBased q1;
+	private QueueReferenceBased q2;
 
+	/** Initialize your Stack. */
+	public Stack() {
+		q1 = new QueueReferenceBased();
+		q2 = new QueueReferenceBased();
+	}
 
-    /** Push element x onto stack. */
-    public void push(Object x) {
-        //Your implementation of push( ) operation goes here.
-    }
-    
+	/** Push element x onto stack. */
+	public void push(Object x) {
+		q2.enqueue(x);
+		while (!q1.isEmpty()) {
+			q2.enqueue(q1.peek());
+			q1.dequeue();
+		}
+		q1 = q2;
+		q2 = new QueueReferenceBased();
+		System.out.println(q1.peek());
+	}
+
 	/** Pop the latest added element out of the stack. */
-    public Object pop() {
-		//Your implementation of pop( ) operation goes here.
+	public Object pop() {
+		return q1.dequeue();
+	}
 
-    }
-    
 	/** Check if the stack is empty. */
-    public boolean isEmpty() {
-		//Your implementation of isEmpty( )  goes here.
-    }
+	public boolean isEmpty() {
+		if (q1.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	/** Print out the queue and clear it.
+	 *  Preconditions: none
+	 *  Postconditions: print out every item in the queue and clears it.
+	 */
+	public void printQNC() {
+		while (!q1.isEmpty()) {
+			System.out.print(q1.peek());
+			q1.dequeue();
+		}
+	}
 
 }
